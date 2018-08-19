bb6e813b001abed613e63776f0c02fe01c1db90c
Leandro Scardua <leandroscar@gmail.com>
2018-08-19 19:28:19 -0400

    Update PS-create-sql-login.ps1

--- PS-create-sql-login.ps1
+++ PS-create-sql-login.ps1
@@ -1,8 +1,10 @@
-
+$login = "user1"
+$password = -join ((48..57) + (97..122) | Get-Random -Count 8 | % {[char]$_})
+$sqlserver = "sql-server-name"
 
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
 [System.Reflection.Assembly]::LoadWithPartialName(" Microsoft.SqlServer.Management.Smo.Login")
-$login = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Login -ArgumentList $sqlserver, $cn
+$login = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Login -ArgumentList $sqlserver, $login
 $login.LoginType = "SqlLogin"
 $login.PasswordExpirationEnabled = $false
 $login.PasswordPolicyEnforced = $false
